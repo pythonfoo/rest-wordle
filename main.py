8ebad77c8a87c17e066b7a1aa85bd1a9e9e7801a
Olaf Gladis <hwmrocker@gladis.org>
2022-10-06 22:32:41 +0200

    small updates

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import random
 from pathlib import Path
 
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 words = (Path(__file__).parent / "wordlist.txt").read_text().splitlines()
 secret_word = random.choice(words)
-
+print(secret_word)
 app = FastAPI()
 
 
@@ -21,17 +22,36 @@ async def new_word():
     return {"secret_word": secret_word}
 
 
+@app.get("/wordlist")
+async def wordlist():
+    return {"wordlist": words}
+
+
 @app.get("/guess/{word}")
 async def guess(word: str):
+    word = word.lower()
+
     def generate_mastermind_result(word):
         for idx, c in enumerate(word):
             if c not in secret_word:
-                yield "X"
+                yield "black"
             elif c == secret_word[idx]:
-                yield "G"
+                yield "green"
             else:
-                yield "Y"
+                yield "yellow"
 
-    return {
-        "mastermind_result": "".join(generate_mastermind_result(word)),
-    }
+    if word not in words:
+        return JSONResponse(status_code=404, content={"message": "word not found"})
+    if word == secret_word:
+        return JSONResponse(
+            status_code=200,
+            content={"word": word, "mastermind_result": ["green"] * len(word)},
+        )
+    else:
+        return JSONResponse(
+            status_code=202,
+            content={
+                "word": word,
+                "mastermind_result": list(generate_mastermind_result(word)),
+            },
+        )
